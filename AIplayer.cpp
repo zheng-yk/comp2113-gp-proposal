3ea305eced9afa82a4ed228c5bbb0ea57dc3a0ae
Yike Zheng <71818067+zheng-yk@users.noreply.github.com>
2020-11-18 23:57:14 +0800

    Update AIplayer.cpp

--- AIplayer.cpp
+++ AIplayer.cpp
@@ -1,10 +1,13 @@
 #include "AIplayer.h"
 
+//set the asigned color and name for the AI
 AIplayer::AIplayer(int color, std::string name)
 {
 	this->color = color;
 	this->name = name;
 }
+
+//return the piece's feature of AI, output: color, position
 Chess AIplayer::giveNextChess(const int chesspadstate[15][15])
 {
 	Position position;
