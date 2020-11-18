cde320adce0090ed773c7ecd4229d0f269f8a737
Yemi-Liu <55391688+Yemi-Liu@users.noreply.github.com>
2020-11-18 18:34:44 +0800

    Add files via upload

---
+++ Variables.h
@@ -0,0 +1,22 @@
+#ifndef VARIABLES_H_INCLUDED
+#define VARIABLES_H_INCLUDED
+
+
+extern int chessboard[15][15];//declare a 15*15 array chessboard
+extern char blank[3];
+extern char black_chess[3];
+extern char white_chess[3];
+
+
+///
+extern void initialize_chessboard();
+extern void initialize_sign();
+
+
+
+////////functions in Show.h
+extern void show_chessboard();
+
+
+
+#endif // VARIABLES_H_INCLUDED
---
+++ includes.h
@@ -0,0 +1,33 @@
+#ifndef INCLUDES_H_INCLUDED
+#define INCLUDES_H_INCLUDED
+///////////////////define global variables
+int chessboard[15][15];//declare a 15*15 array chessboard
+char blank[3];
+char black_chess[3];
+char white_chess[3];
+
+
+
+
+//////////////////declare functions
+
+
+
+
+//////////////////include files
+
+
+#include "AI/AI.h"
+#include "AIplayer/AIplayer.h"
+#include "Board/Board.h"
+#include "Chess/Chess.h"
+#include "Game/Game.h"
+#include "Initialize/Initialize.h"
+#include "Judge/Judge.h"
+#include "PersonPlayer/PersonPlayer.h"
+#include "Player/Player.h"
+#include "Show/Show.h"
+
+
+
+#endif // INCLUDES_H_INCLUDED
